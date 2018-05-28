57bf260415cb5d7384bdf11178f7c090a8a449f5
Hugo Sandelius <hugo.sandelius@gmail.com>
2018-05-28 22:20:26 +0200

    Pimp the website

--- 1337/1337.py
+++ 1337/1337.py
@@ -14,6 +14,16 @@ from apscheduler.triggers.cron import CronTrigger
 WEB_API_TOKEN = os.environ["SLACK_API_TOKEN"]
 CACHE_DIR = os.getenv("CACHE_DIR", ".")
 
+EMOJIS = [
+    "https://emoji.slack-edge.com/T0QJDQ4MC/hackerman-matrix/79021d6f4a7c07e4.gif",
+    "https://emoji.slack-edge.com/T0QJDQ4MC/hackerman_triggered/b1951faae08e5b77.gif",
+    "https://emoji.slack-edge.com/T0QJDQ4MC/hackerman/bd0c31e800d5de3f.png",
+    "https://emoji.slack-edge.com/T0QJDQ4MC/neckbeard_borgen/c9b3bdce923311ab.png",
+    "https://emoji.slack-edge.com/T0QJDQ4MC/neckbeard/c8ec7bf188.png"
+]
+
+SCRUB_EMOJI = "https://emoji.slack-edge.com/T0QJDQ4MC/sylten/d99fe0d901f540a2.jpg"
+
 app = Flask(__name__)
 
 def save_top_1337():
@@ -22,10 +32,6 @@ def save_top_1337():
     toplist = get_top_1337();
     with open("1337.json", "w") as toplist_file:
         json.dump(toplist, toplist_file)
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=save_top_1337, trigger=CronTrigger(hour=13, minute=38))
-scheduler.start()
 
 @app.route('/')
 @app.route('/1337/')
@@ -62,11 +68,12 @@ def format_toplist(toplist):
     index = 0
     last_count = None
     for name, count in toplist:
-        if count != last_count:
-            index += 1
+        index += 1
+
         last_count = count
 
-        formatted.append({"index" : index, "name" : name, "count": count})
+        emoji = EMOJIS[index - 1] if index - 1 < len(EMOJIS) else SCRUB_EMOJI
+        formatted.append({"name" : name, "img" : emoji, "count" : count})
     return formatted
 
 def get_top_1337():
@@ -75,7 +82,9 @@ def get_top_1337():
     leetcount = Counter()
     resp = None    
     first_ts = None
+    last_ts = None
     last_leet_date = None
+
     while not resp or resp["has_more"]:
         response = requests.get("https://slack.com/api/channels.history", 
             params={
@@ -88,6 +97,9 @@ def get_top_1337():
         for msg in resp["messages"]:
             first_ts = msg["ts"]
 
+            if not last_ts:
+                last_ts = msg["ts"]
+
             unix_ts = float(first_ts.split(".")[0])
             dt = datetime.fromtimestamp(unix_ts, tz)
 
@@ -97,4 +109,19 @@ def get_top_1337():
                 
         time.sleep(1)
 
+    for name in name_dic.values():
+        if name not in leetcount:
+            leetcount[name] = 0 
+
+    #with open("timestamp", "w") as tsfile:
+    #    tsfile.write(str(last_ts))
+
     return sorted(leetcount.items(), key=itemgetter(1), reverse=True)
+
+
+if not os.path.isfile("1337.json"):
+    save_top_1337()
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=save_top_1337, trigger=CronTrigger(hour=13, minute=38))
+scheduler.start()
