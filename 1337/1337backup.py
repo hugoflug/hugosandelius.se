b1f7664bd1c949b732e5f0116d8b697a2a45b062
Hugo Sandelius <hugo.sandelius@gmail.com>
2018-05-28 22:54:07 +0200

    Reserve scrub emoji for people with 0 points

--- 1337/1337backup.py
+++ 1337/1337backup.py
@@ -8,6 +8,8 @@ from datetime import datetime
 from collections import Counter
 from operator import itemgetter
 from flask import Flask, request, jsonify, render_template, send_from_directory
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
 
 WEB_API_TOKEN = os.environ["SLACK_API_TOKEN"]
 CACHE_DIR = os.getenv("CACHE_DIR", ".")
@@ -24,10 +26,18 @@ SCRUB_EMOJI = "https://emoji.slack-edge.com/T0QJDQ4MC/sylten/d99fe0d901f540a2.jp
 
 app = Flask(__name__)
 
+def save_top_1337():
+    print("Saving toplist to file")
+
+    with open("1337.json", "w") as toplist_file:
+        json.dump(toplist, toplist_file)
+
 @app.route('/')
 @app.route('/1337/')
 def index():
-    return render_template('index.html', toplist=format1337(top1337()))
+    with open("1337.json") as toplist_file:
+        toplist = json.load(toplist_file)
+        return render_template('index.html', toplist=format_toplist(toplist))
 
 @app.route('/1337/static/<path:path>')
 def serve_static(path):
@@ -52,39 +62,29 @@ def get_name_dic():
 
 name_dic = get_name_dic()
 
-def format1337(toplist):
+def format_toplist(toplist):
     formatted = []
     index = 0
     last_count = None
     for name, count in toplist:
         index += 1
 
-        if count == 0:
-            emoji = SCRUB_EMOJI
-        else: 
-            emoji = EMOJIS[index - 1] if index - 1 < len(EMOJIS) else EMOJIS[-1]
+        last_count = count
+
+        emoji = EMOJIS[index - 1] if index - 1 < len(EMOJIS) else SCRUB_EMOJI
         formatted.append({"name" : name, "img" : emoji, "count" : count})
     return formatted
 
-
-def top1337():
+def get_top_1337():
     tz = pytz.timezone("Europe/Stockholm")
 
-    try:
-        with open(CACHE_DIR + "/1337.json") as cache:
-            cache_json = json.load(cache)
-            leetcount = Counter(cache_json["top"])
-            limit_ts = cache_json["ts"]
-    except IOError:
-        leetcount = Counter()
-        limit_ts = "0"
-
+    leetcount = Counter()
     resp = None    
     first_ts = None
     last_ts = None
     last_leet_date = None
 
-    while not resp or (resp["has_more"] and first_ts > limit_ts):
+    while not resp or resp["has_more"]:
         response = requests.get("https://slack.com/api/channels.history", 
             params={
                 "token" : WEB_API_TOKEN,
@@ -97,10 +97,7 @@ def top1337():
             first_ts = msg["ts"]
 
             if not last_ts:
-                last_ts = first_ts
-
-            if first_ts <= limit_ts:
-                break
+                last_ts = msg["ts"]
 
             unix_ts = float(first_ts.split(".")[0])
             dt = datetime.fromtimestamp(unix_ts, tz)
@@ -108,18 +105,22 @@ def top1337():
             if "user" in msg and dt.time().hour == 13 and dt.time().minute == 37 and dt.date() != last_leet_date:
                 last_leet_date = dt.date()
                 leetcount[name_dic[msg["user"]]] += 1
-
+                
         time.sleep(1)
-
-    with open(CACHE_DIR + "/1337.json", "w") as cache:
-        cache_data = {
-            "ts" : last_ts,
-            "top" : dict(leetcount)    
-        }
-        json.dump(cache_data, cache)
 
     for name in name_dic.values():
         if name not in leetcount:
             leetcount[name] = 0 
 
+    #with open("timestamp", "w") as tsfile:
+    #    tsfile.write(str(last_ts))
+
     return sorted(leetcount.items(), key=itemgetter(1), reverse=True)
+
+
+if not os.path.isfile("1337.json"):
+    save_top_1337()
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=save_top_1337, trigger=CronTrigger(hour=13, minute=38))
+scheduler.start()
